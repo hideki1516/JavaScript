c1e1b467d3b976b40e5b2931e8fa5964f565a3f1
Hideki_Yamamoto <hideki_yamamoto@Hideki-YamamotonoMacBook-Pro.local>
2021-08-24 23:10:57 +0900

    JavaScript 課題1-1 修正

---
+++ JS_1-1/main.js
@@ -0,0 +1,14 @@
+const Fizz = 3;
+const Buzz = 5;
+
+for (let i = 1; i <= 100; i++) {
+    if(i % Fizz === 0 && i % Buzz === 0){
+        console.log('FizzBuzz ' + i);
+    } else if(i % Buzz === 0){
+        console.log('Buzz ' + i);
+    } else if(i % Fizz === 0){
+        console.log('Fizz ' +i);
+    } else {
+        console.log();
+    }
+}
